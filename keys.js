d485fb05d9400ef4d03a17c2bd5f93a7066aa730
Forrest Eastland <feastland4@gmail.com>
2017-10-09 18:43:25 -0500

    first update

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'bGgSU0qyC2nFkHzlZ0sbmePEk',
+  consumer_secret: 'vsA6VY7QriMcm9KE5B5kCNFWz4wbVVwPgj1hw2UbCHw4KxAVnW',
+  access_token_key: '917522168065482753-1LYzsW34LzudWxrmPTr0OfNhNZk0kPz',
+  access_token_secret: 'SMOTrHqFZxsVSzq0PyuXi5wdM3eWfAhvICPLsDwaYMpSq',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,5 @@
+// Packages
+var keys = require("./keys.js");
+var request = require("request");
+var twitter = require("twitter");
+
